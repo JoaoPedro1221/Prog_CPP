e57c510e1fb9d8621d9c8c57f8b20282c51039b6
João Pedro Furquim Marques <joaopedrofm@uol.com.br>
2020-05-15 17:04:55 -0300

    Create Le_AB_Imp_BA.cpp

---
+++ Diversos/Le_AB_Imp_BA.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+main()
+
+{
+   float A, B, C;
+   cout<<"Insira A: ";
+   cin>>A;
+   cout<<"Insira B: ";
+   cin>>B;
+
+   system("cls"); //Limpar dados da Tela
+
+   cout<<"A: "<<A<<endl;
+   cout<<"B: "<<B<<endl;
+
+   return 0;
+}

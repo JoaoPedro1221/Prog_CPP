89131f7fd8b6ff2b261259851ad5d70e864cb234
João Pedro Furquim Marques <joaopedrofm@uol.com.br>
2020-05-18 00:27:23 -0300

    Create Tabuada.cpp

---
+++ Diversos/Tabuada.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+
+main()
+
+{
+   float NumInicial;
+   cout<<"Qual o numero que voce quer calcular a tabuada: ";
+   cin>>NumInicial;
+   system("cls");
+   cout<<NumInicial<<"x1= "<<NumInicial<<endl;
+   cout<<NumInicial<<"x2= "<<NumInicial*2<<endl;
+   cout<<NumInicial<<"x3= "<<NumInicial*3<<endl;
+   cout<<NumInicial<<"x4= "<<NumInicial*4<<endl;
+   cout<<NumInicial<<"x5= "<<NumInicial*5<<endl;
+   cout<<NumInicial<<"x6= "<<NumInicial*6<<endl;
+   cout<<NumInicial<<"x7= "<<NumInicial*7<<endl;
+   cout<<NumInicial<<"x8= "<<NumInicial*8<<endl;
+   cout<<NumInicial<<"x9= "<<NumInicial*9<<endl;
+   cout<<NumInicial<<"x10= "<<NumInicial*10<<endl;
+   return 0;
+}
+

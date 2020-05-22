a6504e1236ab436ddd2f196b3071e91d7b476d15
João Pedro Furquim Marques <joaopedrofm@uol.com.br>
2020-05-21 23:53:43 -0300

    Create c6 ex3.cpp

---
+++ Lista6/c6 ex3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int b, m;
+    for(int a=0; a<20; a++)
+    {
+        cin>>b;
+
+        if (b<m)
+        {
+            m=b;
+        }
+    }
+    cout<<m;
+}
+
+
